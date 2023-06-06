420ee6cd8e68b2627f9af5ba83f09c4186e4d366
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-06-06 21:25:17 +0200

    create avatar and logo components

---
+++ app/components/navbar/logo.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+
+const Logo = () => {
+  return (
+    <Image
+      src="/images/logo.png"
+      height="100"
+      width="100"
+      alt="Logo"
+      className="hidden md:block cursor-pointer"
+    />
+  );
+};
+
+export default Logo;
