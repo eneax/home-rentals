aea4d92734d61a0ddc3153171bfdb99c0807ec11
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-06-07 20:04:10 +0200

    create navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Nunito } from "next/font/google";
 
 import "./globals.css";
 
+import Navbar from "@/components/navbar/navbar";
+
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
@@ -17,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Container from "@/components/container";
+import Logo from "@/components/navbar/logo";
+import Search from "@/components/navbar/search";
+import UserMenu from "@/components/navbar/user-menu";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
