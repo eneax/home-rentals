6a567fbeb17aa941de5810f2bd48e7193be62c5b
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-06-11 16:58:11 +0200

    create useCountries custom hook

---
+++ hooks/use-countries.ts
@@ -0,0 +1,23 @@
+import countries from "world-countries";
+
+const formattedCountries = countries.map((country) => ({
+  label: country.name.common,
+  value: country.cca2,
+  flag: country.flag,
+  latlng: country.latlng,
+  region: country.region,
+}));
+
+const useCountries = () => {
+  const getAll = () => formattedCountries;
+
+  const getByValue = (value: string) =>
+    formattedCountries.find((country) => country.value === value);
+
+  return {
+    getAll,
+    getByValue,
+  };
+};
+
+export default useCountries;
