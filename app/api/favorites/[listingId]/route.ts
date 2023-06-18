be8730013b1aae0a893970144cb8b9935fbc1426
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-06-18 11:38:45 +0200

    implement POST function for favorite listing route handler

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/get-current-user";
+import prisma from "@/lib/prismadb";
+
+interface IParams {
+  listingId?: string;
+}
+
+export async function POST(request: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid listing ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+  favoriteIds.push(listingId);
+
+  const user = await prisma.user.update({
+    where: { id: currentUser.id },
+    data: { favoriteIds },
+  });
+
+  return NextResponse.json(user);
+}
+
