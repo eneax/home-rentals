d92dc0ad629220f2f0cfb575876c124e467541e9
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-06-20 20:42:09 +0200

    implement API for deleting a reservation

---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/lib/prismadb";
+import getCurrentUser from "@/app/actions/get-current-user";
+
+interface IParams {
+  reservationId?: string;
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { reservationId } = params;
+
+  if (!reservationId || typeof reservationId !== "string") {
+    throw new Error("Invalid reservation ID");
+  }
+
+  const reservation = await prisma.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [
+        { userId: currentUser.id },
+        {
+          listing: {
+            userId: currentUser.id,
+          },
+        },
+      ],
+    },
+  });
+
+  return NextResponse.json(reservation);
+}
