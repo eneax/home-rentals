07a93b7d5d87eff7e25b296688a510a5b8dec610
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-06-21 21:19:40 +0200

    link properties page to main nav dropdown

--- components/navbar/user-menu.tsx
+++ components/navbar/user-menu.tsx
@@ -71,7 +71,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   label="My reservations"
                   onClick={() => router.push("/reservations")}
                 />
-                <MenuItem label="My properties" onClick={() => {}} />
+                <MenuItem
+                  label="My properties"
+                  onClick={() => router.push("/properties")}
+                />
                 <MenuItem label="Airbnb my home" onClick={rentModal.onOpen} />
 
                 <hr />
