ba408b951f4a5a8d677b11934a74274603727fbe
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-06-24 16:31:16 +0200

    use SkewLoader for loading state UI

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { PuffLoader } from "react-spinners";
+import { SkewLoader } from "react-spinners";
 
 const Loader = () => {
   return (
     <div className="h-[70vh] flex flex-col justify-center items-center">
-      <PuffLoader size={100} color="red" />
+      <SkewLoader size={50} color="#666" />
     </div>
   );
 };
